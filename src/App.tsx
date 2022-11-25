66918a7b0d10a2c5f4d76c0d74c7f32f010a849f
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-11-25 13:37:01 -0300

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import Button from "./components/Button";
+
+import Form from "./components/Form";
+import List from "./components/List";
 
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Form />
+      <List />
     </div>
   );
 }
