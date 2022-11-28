5d6271a6fba81b32c851f8220545f428eab5c8aa
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-11-28 15:25:53 -0300

    add stopwatch

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,10 +1,23 @@
 import style from './Stopwatch.module.scss'
 import Button from "../Button";
 import Clock from "./Clock";
-import { tempoParaSegundos } from '../../common/utils/date';
+import { tempoParaSegundos } from '../../common/utils/time';
+import { ITarefa } from '../../types/tarefa';
+import { useEffect, useState } from 'react';
 
-export default function Stopwatch() {
-    console.log('conversao:', tempoParaSegundos('01:01:01'))
+interface Props {
+    selecionado: ITarefa | undefined;
+}
+
+export default function Stopwatch({ selecionado }: Props) {
+
+    const [tempo, setTempo] = useState<number>();
+
+   useEffect(() => {
+    if(selecionado?.tempo){
+        setTempo(tempoParaSegundos(selecionado.tempo))
+    }}, [selecionado])
+   
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -27,7 +27,7 @@ function App() {
       tarefas={tarefas} 
       selecionaTarefa={selecionaTarefa}
       />
-      <Stopwatch />
+      <Stopwatch selecionado={selecionado} />
     </div>
   );
 }
